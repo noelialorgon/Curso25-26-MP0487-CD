03c4a9f62495e2844a8962588042fecf103a1786
noelialorgon <noelialorgon@iesrodeira.gal>
2025-11-04 18:17:11 +0100

    Cambios para la tarea-01 con git

--- UNIDAD-01/CODIGO-PRUEBAS/Java/Ejemplos02/fig07_02/InitArray.java
+++ UNIDAD-01/CODIGO-PRUEBAS/Java/Ejemplos02/fig07_02/InitArray.java
@@ -4,13 +4,13 @@
 public class InitArray {
    public static void main(String[] args) {
       // declare variable array and initialize it with an array object
-      int[] array = new int[10]; // create the array object           
+      int[] arrayNoelia = new int[10]; // create the array object           
 
       System.out.printf("%s%8s%n", "Index noelia", "Value"); // column headings
    
       // output each array element's value                      
-      for (int counter = 0; counter < array.length; counter++) {
-         System.out.printf("%5d%8d%n", counter, array[counter]);
+      for (int counter = 0; counter < arrayNoelia.length; counter++) {
+         System.out.printf("%5d%8d%n", counter, arrayNoelia[counter]);
       }                                                         
    } 
 } 
